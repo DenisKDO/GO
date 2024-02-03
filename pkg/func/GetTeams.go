86201129002a6413ed7153dff21069fdac77c072
DenisKDO <den_kim@kbtu.kz>
2024-02-03 14:55:29 +0600

    Deleting useless slice in GetTeams func

--- pkg/func/GetTeams.go
+++ pkg/func/GetTeams.go
@@ -5,10 +5,6 @@ import (
 	"net/http"
 )
 
-type countries struct {
-	Name string
-}
-
 func GetTeams(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "All national teams that my API have:\n-Russia\n-Japan")
 }
